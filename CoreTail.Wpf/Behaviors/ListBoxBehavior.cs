9fd65850187e07547c3ab5a101c351a016c97b50
Martin Pozor <martin.pozor@pozyx.com>
2017-04-05 20:55:07 +0100

    added auto scroll behavior (WPF)

---
+++ CoreTail.Wpf/Behaviors/ListBoxBehavior.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CoreTail.Wpf.Behaviors
+{
+    // from http://druss.co/2013/10/wpf-auto-scroll-behavior-for-listbox/
+    public static class ListBoxBehavior
+    {
+        public static readonly DependencyProperty AutoScrollProperty = DependencyProperty.RegisterAttached(
+            "AutoScroll",
+            typeof(bool),
+            typeof(System.Windows.Controls.ListBox),
+            new PropertyMetadata(false));
+
+        public static readonly DependencyProperty AutoScrollHandlerProperty =
+            DependencyProperty.RegisterAttached(
+                "AutoScrollHandler",
+                typeof(AutoScrollHandler),
+                typeof(System.Windows.Controls.ListBox));
+
+        public static bool GetAutoScroll(System.Windows.Controls.ListBox instance)
+        {
+            return (bool)instance.GetValue(AutoScrollProperty);
+        }
+
+        public static void SetAutoScroll(System.Windows.Controls.ListBox instance, bool value)
+        {
+            var oldHandler = (AutoScrollHandler)instance.GetValue(AutoScrollHandlerProperty);
+            if (oldHandler != null)
+            {
+                oldHandler.Dispose();
+                instance.SetValue(AutoScrollHandlerProperty, null);
+            }
+
+            instance.SetValue(AutoScrollProperty, value);
+            if (value)
+                instance.SetValue(AutoScrollHandlerProperty, new AutoScrollHandler(instance));
+        }
+
+        private class AutoScrollHandler : DependencyObject, IDisposable
+        {
+            public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register(
+                "ItemsSource",
+                typeof(IEnumerable),
+                typeof(AutoScrollHandler),
+                new FrameworkPropertyMetadata(
+                    null,
+                    FrameworkPropertyMetadataOptions.None,
+                    ItemsSourcePropertyChanged));
+
+            private readonly System.Windows.Controls.ListBox _target;
+
+            public AutoScrollHandler(System.Windows.Controls.ListBox target)
+            {
+                _target = target;
+                var binding = new Binding("ItemsSource") { Source = _target };
+                BindingOperations.SetBinding(this, ItemsSourceProperty, binding);
+            }
+
+            public void Dispose()
+            {
+                BindingOperations.ClearBinding(this, ItemsSourceProperty);
+            }
+
+            public IEnumerable ItemsSource
+            {
+                get => (IEnumerable)GetValue(ItemsSourceProperty);
+                set => SetValue(ItemsSourceProperty, value);
+            }
+
+            private static void ItemsSourcePropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+            {
+                ((AutoScrollHandler)o).ItemsSourceChanged((IEnumerable)e.OldValue, (IEnumerable)e.NewValue);
+            }
+
+            private void ItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
+            {
+                var collection = oldValue as INotifyCollectionChanged;
+                if (collection != null)
+                    collection.CollectionChanged -= CollectionChangedEventHandler;
+
+                collection = newValue as INotifyCollectionChanged;
+                if (collection != null)
+                    collection.CollectionChanged += CollectionChangedEventHandler;
+            }
+
+            private void CollectionChangedEventHandler(object sender, NotifyCollectionChangedEventArgs e)
+            {
+                if (e.Action != NotifyCollectionChangedAction.Add || e.NewItems == null || e.NewItems.Count < 1)
+                {
+                    return;
+                }
+
+                _target.ScrollIntoView(e.NewItems[e.NewItems.Count - 1]);
+            }
+        }
+    }
+}
--- CoreTail.Wpf/MainWindow.xaml.cs
+++ CoreTail.Wpf/MainWindow.xaml.cs
@@ -1,50 +1,18 @@
-﻿using System;
-using System.Collections.Specialized;
-using System.Windows.Controls;
-using System.Windows.Media;
-using CoreTail.Shared;
+﻿using CoreTail.Shared;
 using CoreTail.Wpf.Shared;
 
 namespace CoreTail.Wpf
 {
     public partial class MainWindow
     {
-        private readonly Lazy<ScrollViewer> _scrollViewer;
-
-        private IDispatcher _dispatcher;
-        private MainWindowViewModel _viewModel;
-
         public MainWindow()
         {
             InitializeComponent();
 
-            _scrollViewer = new Lazy<ScrollViewer>(() =>
-            {
-                if (VisualTreeHelper.GetChildrenCount(LogContentListBox) > 0)
-                {
-                    var border = (Border)VisualTreeHelper.GetChild(LogContentListBox, 0);
-                    var scrollViewer = (ScrollViewer)VisualTreeHelper.GetChild(border, 0);
-                    return scrollViewer;
-                }
-                return null;
-            });
+            var viewModel = new MainWindowViewModel(new Dispatcher(Dispatcher));
+            viewModel.Initialize();
 
-            _dispatcher = new Dispatcher(Dispatcher);
-            _viewModel = new MainWindowViewModel(_dispatcher);
-            _viewModel.Initialize();
-
-            DataContext = _viewModel;
-            _viewModel.LogContent.CollectionChanged += LogContent_CollectionChanged;
-        }
-
-        private void LogContent_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            ScrollToBottom();
-        }
-
-        private void ScrollToBottom()
-        {
-            _scrollViewer.Value.ScrollToBottom();
+            DataContext = viewModel;
         }
     }
 }
